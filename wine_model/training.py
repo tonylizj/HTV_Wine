10c4ae9d97e378b070b7095d01214a0dea34bb36
Tony Li <tonyli8101@gmail.com>
2020-02-16 04:05:23 -0500

    more fixes, training off checkpoints

---
+++ wine_model/training.py
@@ -0,0 +1,311 @@
+# %%
+
+import argparse
+import json
+import os
+import numpy as np
+import tensorflow as tf
+import time
+import tqdm
+from tensorflow.core.protobuf import rewriter_config_pb2
+
+# %%
+
+import model, sample, encoder
+from load_dataset import load_dataset, Sampler
+from accumulate import AccumulatingOptimizer
+import memory_saving_gradients
+
+# %%
+
+CHECKPOINT_DIR = 'checkpoint'
+SAMPLE_DIR = 'samples'
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+
+
+# %%
+
+def maketree(path):
+    try:
+        os.makedirs(path)
+    except:
+        pass
+
+
+# %%
+
+def randomize(context, hparams, p):
+    if p > 0:
+        mask = tf.random.uniform(shape=tf.shape(context)) < p
+        noise = tf.random.uniform(shape=tf.shape(context), minval=0, maxval=hparams.n_vocab, dtype=tf.int32)
+        return tf.where(mask, noise, context)
+    else:
+        return context
+
+
+# %%
+
+class args_passed():
+    def __init__(self):
+        self.dataset = 'wine_data.npz'
+        self.model_name = 'wine_model_early_test'
+        self.combine = 50000
+        self.encoding = 'utf-8'
+
+        self.batch_size = 1
+        self.learning_rate = 0.00002
+        self.accumulate_gradients = 1
+        self.memory_saving_gradients = False
+        self.only_train_transformer_layers = False
+        self.optimizer = 'adam'
+        self.noise = 0.0
+
+        self.top_k = 40
+        self.top_p = 0.0
+
+        self.restore_from = 'latest'
+        self.run_name = 'run1'
+        self.sample_every = 100
+        self.sample_length = 1023
+        self.sample_num = 1
+        self.save_every = 10
+
+        self.val_dataset = None
+        self.val_batch_size = 2
+        self.val_batch_count = 40
+        self.val_every = 0
+
+
+args = args_passed()
+
+print(args.model_name)
+print(args.dataset)
+
+# %%
+
+def main():
+    enc = encoder.get_encoder(args.model_name)
+    hparams = model.default_hparams()
+    with open(os.path.join('models', args.model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if args.sample_length > hparams.n_ctx:
+        raise ValueError(
+            "Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    if args.model_name == '345M':
+        args.memory_saving_gradients = True
+        if args.optimizer == 'adam':
+            args.only_train_transformer_layers = True
+
+    config = tf.ConfigProto()
+    config.gpu_options.allow_growth = True
+    config.graph_options.rewrite_options.layout_optimizer = rewriter_config_pb2.RewriterConfig.OFF
+    with tf.Session(config=config) as sess:
+        context = tf.placeholder(tf.int32, [args.batch_size, None])
+        context_in = randomize(context, hparams, args.noise)
+        output = model.model(hparams=hparams, X=context_in)
+        loss = tf.reduce_mean(
+            tf.nn.sparse_softmax_cross_entropy_with_logits(
+                labels=context[:, 1:], logits=output['logits'][:, :-1]))
+
+        if args.val_every > 0:
+            val_context = tf.placeholder(tf.int32, [args.val_batch_size, None])
+            val_output = model.model(hparams=hparams, X=val_context)
+            val_loss = tf.reduce_mean(
+                tf.nn.sparse_softmax_cross_entropy_with_logits(
+                    labels=val_context[:, 1:], logits=val_output['logits'][:, :-1]))
+            val_loss_summary = tf.summary.scalar('val_loss', val_loss)
+
+        tf_sample = sample.sample_sequence(
+            hparams=hparams,
+            length=args.sample_length,
+            context=context,
+            batch_size=args.batch_size,
+            temperature=1.0,
+            top_k=args.top_k,
+            top_p=args.top_p)
+
+        all_vars = [v for v in tf.trainable_variables() if 'model' in v.name]
+        train_vars = [v for v in all_vars if '/h' in v.name] if args.only_train_transformer_layers else all_vars
+
+        if args.optimizer == 'adam':
+            opt = tf.train.AdamOptimizer(learning_rate=args.learning_rate)
+        elif args.optimizer == 'sgd':
+            opt = tf.train.GradientDescentOptimizer(learning_rate=args.learning_rate)
+        else:
+            exit('Bad optimizer:', args.optimizer)
+
+        if args.accumulate_gradients > 1:
+            if args.memory_saving_gradients:
+                exit("Memory saving gradients are not implemented for gradient accumulation yet.")
+            opt = AccumulatingOptimizer(
+                opt=opt,
+                var_list=train_vars)
+            opt_reset = opt.reset()
+            opt_compute = opt.compute_gradients(loss)
+            opt_apply = opt.apply_gradients()
+            summary_loss = tf.summary.scalar('loss', opt_apply)
+        else:
+            if args.memory_saving_gradients:
+                opt_grads = memory_saving_gradients.gradients(loss, train_vars)
+            else:
+                opt_grads = tf.gradients(loss, train_vars)
+            opt_grads = list(zip(opt_grads, train_vars))
+            opt_apply = opt.apply_gradients(opt_grads)
+            summary_loss = tf.summary.scalar('loss', loss)
+
+        summary_lr = tf.summary.scalar('learning_rate', args.learning_rate)
+        summaries = tf.summary.merge([summary_lr, summary_loss])
+
+        summary_log = tf.summary.FileWriter(
+            os.path.join(CHECKPOINT_DIR, args.run_name))
+
+        saver = tf.train.Saver(
+            var_list=all_vars,
+            max_to_keep=5,
+            keep_checkpoint_every_n_hours=2)
+        sess.run(tf.global_variables_initializer())
+
+        if args.restore_from == 'latest':
+            ckpt = tf.train.latest_checkpoint(
+                os.path.join(CHECKPOINT_DIR, args.run_name))
+            if ckpt is None:
+                # Get fresh GPT weights if new run.
+                ckpt = tf.train.latest_checkpoint(
+                    os.path.join('models', args.model_name))
+        elif args.restore_from == 'fresh':
+            ckpt = tf.train.latest_checkpoint(
+                os.path.join('models', args.model_name))
+        else:
+            ckpt = tf.train.latest_checkpoint(args.restore_from)
+        print('Loading checkpoint', ckpt)
+        saver.restore(sess, ckpt)
+
+        print('Loading dataset...')
+        chunks = load_dataset(enc, args.dataset, args.combine, encoding=args.encoding)
+        data_sampler = Sampler(chunks)
+        if args.val_every > 0:
+            if args.val_dataset:
+                val_chunks = load_dataset(enc, args.val_dataset, args.combine, encoding=args.encoding)
+            else:
+                val_chunks = chunks
+        print('dataset has', data_sampler.total_size, 'tokens')
+        print('Training...')
+
+        if args.val_every > 0:
+            # Sample from validation set once with fixed seed to make
+            # it deterministic during training as well as across runs.
+            val_data_sampler = Sampler(val_chunks, seed=1)
+            val_batches = [[val_data_sampler.sample(1024) for _ in range(args.val_batch_size)]
+                           for _ in range(args.val_batch_count)]
+
+        counter = 1
+        counter_path = os.path.join(CHECKPOINT_DIR, args.run_name, 'counter')
+        if os.path.exists(counter_path):
+            # Load the step number if we're resuming a run
+            # Add 1 so we don't immediately try to save again
+            with open(counter_path, 'r') as fp:
+                counter = int(fp.read()) + 1
+
+        def save():
+            maketree(os.path.join(CHECKPOINT_DIR, args.run_name))
+            print(
+                'Saving',
+                os.path.join(CHECKPOINT_DIR, args.run_name,
+                             'model-{}').format(counter))
+            saver.save(
+                sess,
+                os.path.join(CHECKPOINT_DIR, args.run_name, 'model'),
+                global_step=counter)
+            with open(counter_path, 'w') as fp:
+                fp.write(str(counter) + '\n')
+
+        def generate_samples():
+            print('Generating samples...')
+            context_tokens = data_sampler.sample(1)
+            all_text = []
+            index = 0
+            while index < args.sample_num:
+                out = sess.run(
+                    tf_sample,
+                    feed_dict={context: args.batch_size * [context_tokens]})
+                for i in range(min(args.sample_num - index, args.batch_size)):
+                    text = enc.decode(out[i])
+                    text = '======== SAMPLE {} ========\n{}\n'.format(
+                        index + 1, text)
+                    all_text.append(text)
+                    index += 1
+            print(text)
+            maketree(os.path.join(SAMPLE_DIR, args.run_name))
+            with open(
+                    os.path.join(SAMPLE_DIR, args.run_name,
+                                 'samples-{}').format(counter), 'w', encoding=args.encoding) as fp:
+                fp.write('\n'.join(all_text))
+
+        def validation():
+            print('Calculating validation loss...')
+            losses = []
+            for batch in tqdm.tqdm(val_batches):
+                losses.append(sess.run(val_loss, feed_dict={val_context: batch}))
+            v_val_loss = np.mean(losses)
+            v_summary = sess.run(val_loss_summary, feed_dict={val_loss: v_val_loss})
+            summary_log.add_summary(v_summary, counter)
+            summary_log.flush()
+            print(
+                '[{counter} | {time:2.2f}] validation loss = {loss:2.2f}'
+                    .format(
+                    counter=counter,
+                    time=time.time() - start_time,
+                    loss=v_val_loss))
+
+        def sample_batch():
+            return [data_sampler.sample(1024) for _ in range(args.batch_size)]
+
+        avg_loss = (0.0, 0.0)
+        start_time = time.time()
+
+        try:
+            while True:
+                if counter % args.save_every == 0:
+                    save()
+                if counter % args.sample_every == 0:
+                    generate_samples()
+                if args.val_every > 0 and (counter % args.val_every == 0 or counter == 1):
+                    validation()
+
+                if args.accumulate_gradients > 1:
+                    sess.run(opt_reset)
+                    for _ in range(args.accumulate_gradients):
+                        sess.run(
+                            opt_compute, feed_dict={context: sample_batch()})
+                    (v_loss, v_summary) = sess.run((opt_apply, summaries))
+                else:
+                    (_, v_loss, v_summary) = sess.run(
+                        (opt_apply, loss, summaries),
+                        feed_dict={context: sample_batch()})
+
+                summary_log.add_summary(v_summary, counter)
+
+                avg_loss = (avg_loss[0] * 0.99 + v_loss,
+                            avg_loss[1] * 0.99 + 1.0)
+
+                print(
+                    '[{counter} | {time:2.2f}] loss={loss:2.2f} avg={avg:2.2f}'
+                        .format(
+                        counter=counter,
+                        time=time.time() - start_time,
+                        loss=v_loss,
+                        avg=avg_loss[0] / avg_loss[1]))
+
+                counter += 1
+        except KeyboardInterrupt:
+            print('interrupted')
+            save()
+
+
+# %%
+
+main()
+
